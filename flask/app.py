9b209be0bfb2c60b0ec0523cc61006cd81df222b
Lubomir Nedbal <Lopliok@users.noreply.github.com>
2025-01-16 10:03:13 +0100

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,9 @@
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Cesta k souboru pro ukládání záznamů
 DATA_FILE = 'records.json'
 
